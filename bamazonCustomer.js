596488c82301453881e9b9d28678a938765f5ea8
alyciamriley <alyciarileym@gmail.com>
2018-02-17 17:14:07 -0600

    product list works!

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,22 +2,34 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
-    host: "local host",
-    port: 3306,
-    user: "root",
-    password: "Jonsnow123",
-    database: "bamazon_db"
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Jonsnow123",
+  database: "bamazon_db"
 });
 
-connection.connect(function(err){
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    start();
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  start();
 });
 
 function start() {
-    connection.query("SELECT * FROM products", 
-function(err, res) {
-    console.log(res);
+  connection.query(
+    "SELECT item_id, product_name, price FROM products",
+    function(err, res) {
+      for (var i = 0; i < res.length; i++) {
+        console.log(
+          "Item ID: " +
+            res[i].item_id +
+            "\nProduct: " +
+            res[i].product_name +
+            "\nPrice: " +
+            res[i].price +
+            "\n================================="
+        );
+      }
+    }
+  );
 }
-    )};
