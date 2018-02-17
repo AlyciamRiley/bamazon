ad6b09706e9417b80f37b4fa27599697ea3b6c5b
alyciamriley <alyciarileym@gmail.com>
2018-02-17 17:40:12 -0600

    user prompt is working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,7 +12,8 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
-  start();
+//   start();
+  promptUser();
 });
 
 function start() {
@@ -32,4 +33,30 @@ function start() {
       }
     }
   );
+}
+
+function promptUser(){
+    inquirer
+    .prompt({
+        name: "productID",
+        type: "input",
+        message: "What is the ID number of the product you want to buy?"
+    })
+    .then(function(answer){
+        var query = "SELECT item_id, product_name, price FROM products WHERE ?";
+        connection.query(query, { item_id: answer.productID }, 
+        function(err, res) { 
+            for (var i = 0; i < res.length; i++) {
+                console.log(
+                  "Item ID: " +
+                    res[i].item_id +
+                    "\nProduct: " +
+                    res[i].product_name +
+                    "\nPrice: " +
+                    res[i].price +
+                    "\n================================="
+                );
+              }
+        })
+    })
 }
