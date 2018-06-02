8e5010bc5fbb76c0c7da4071b13996dacdad02e0
alyciamriley <alyciarileym@gmail.com>
2018-06-02 13:21:10 -0500

    added Readme

--- README.md
+++ README.md
@@ -1,18 +1,33 @@
-# bamazon
-This is Homework #12 for the University of Minnesota Coding Bootcamp.  It is a CLI application that uses a MySQL database as well as Node.js to create an online store.
+
+## Bamazon
+This application is an Amazon-like storefront that utilizes a mySQL database to track inventory. The app will take in orders from customers and deplete stock from the store's inventory.
 
 
-Click here https://drive.google.com/file/d/1hE047Xbah28gF0h7-aS5f6-_1NZMN9bJ/view   to watch a video walkthrough of the application.
+## View video of application
+https://drive.google.com/file/d/1hE047Xbah28gF0h7-aS5f6-_1NZMN9bJ/view?usp=sharing
 
-To use the application, follow these steps.
-1.  Open up GitBash or terminal.
-2.  Navigate to the file where the application is located.
-3.  Type in "Node bamazoncustomer"
-4.  You will be prompted whether or not you want to shop.
-5. If you choose "YES", the list of available products will be displayed.
-6.  If you choose no, you will be prompted to answer again.
-7.  Once the products are displayed, please enter the Item ID and how many you would like to purchase.
-8.  If there are enough items in stock, your total will be displayed and your order will be complete.  You will then be prompted whether or not you would like to shop for more items.
-9.  If there are not enough items in stock, the process will start over. 
+## Technologies Used
+- mySQL
+- Node.Js
+- Inquirer 
 
-This application was created by Alycia Riley.  
+## Screenshots
+
+![Main](images/screenshot.PNG)
+
+
+## Built With
+
+- Brackets- Text Editor
+
+## Future Development
+
+- Create a manager view that allows for user authentication and the ability to add inventory.
+
+
+
+## Authors
+
+* Alycia Riley
+* [www.code-retro.com](http://www.code-retro.com)
+
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -21,7 +21,7 @@ function start() {
     .prompt({
       name: "listProducts",
       type: "rawlist",
-      message: "Are you ready to shop? ", 
+      message: "Are you ready to shop? ",
       choices: ["YES", "NO"]
     })
     .then(function(answer) {
@@ -38,17 +38,14 @@ function start() {
 
 function listProducts() {
   connection.query(
-    //querys the database for the item id, name of the product
     "SELECT item_id, stock_quantity, product_name, price FROM products",
-    //res is the response variable that holds the data from the table
     function(err, res) {
-      //loops through the results to pull product information- stops once it has reached the end of the list.
       for (var i = 0; i < res.length; i++) {
         console.log(
           "Item ID: " +
-          
             res[i].item_id +
-            "\nNumber in Stock: " + res[i].stock_quantity + 
+            "\nNumber in Stock: " +
+            res[i].stock_quantity +
             "\nProduct: " +
             res[i].product_name +
             "\nPrice: " +
@@ -58,7 +55,7 @@ function listProducts() {
       }
       promptUser();
     }
-  );  
+  );
 }
 
 //===========prompt user function begins============
@@ -78,59 +75,49 @@ function promptUser() {
       }
     ])
     .then(function(answer) {
-      //querys the database for all data
       var query = "SELECT * FROM products WHERE ?";
-      //attaches the user answer to the item_id from the database and then runs a function on that data
       connection.query(query, { item_id: answer.productID }, function(
         err,
         res
       ) {
-  
         var unitsAvailable;
         var unitPrice;
-        //units requested is pulled from the inquirer prompt
         var unitsRequested = answer.stockQuantity;
-        //this loop is pulling from the data stored in res, which is the data from the DB
         for (var i = 0; i < res.length; i++) {
-          //the unitsAvailable var is being updated with the data from the for look- which is the response from stock_quantity entry. 
           unitsAvailable = res[i].stock_quantity;
-          //unitPrice is doing the same thing as units Available
           unitPrice = res[i].price;
         }
-      
-        //running if then statement on the above data.  if the answer to the units requested inquirer prompt is less than or equal to the units available (which is defined above), then the table needs to be updated with the new number
-        if(answer.stockQuantity <= unitsAvailable){
-          //create variable where units requested is subtracted from ode units available
-          
-          //table is being updated.  
+
+        if (answer.stockQuantity <= unitsAvailable) {
           var query = "UPDATE products SET ? WHERE ?";
           connection.query(
-            query, 
+            query,
             [
               {
-                stock_quantity: unitsAvailable-unitsRequested 
+                stock_quantity: unitsAvailable - unitsRequested
               },
               {
                 item_id: answer.productID
               }
             ],
-            function(err){
-              if(err){
+            function(err) {
+              if (err) {
                 console.log(err);
               }
               console.log("order has been placed");
-              //to fixed rounds the decimals so they don't all display.  the (2) indicates how many decimal places there should be.
-            console.log("Total for your purchase is: " + (unitPrice * answer.stockQuantity).toFixed(2));
-            shopAgain();
+              console.log(
+                "Total for your purchase is: " +
+                  (unitPrice * answer.stockQuantity).toFixed(2)
+              );
+              shopAgain();
             }
-            
-            
           );
-      } else {
-          console.log("Sorry, we do not currently have enough stock to place your order.");
+        } else {
+          console.log(
+            "Sorry, we do not currently have enough stock to place your order."
+          );
           start();
         }
-
       });
     });
 }
@@ -151,4 +138,3 @@ function shopAgain() {
       }
     });
 }
-
